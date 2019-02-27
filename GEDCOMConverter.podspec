2ba9ccb3c93247aaa82c7f35e82ccd6cc4e32f67
Craig Grummitt <craiggrummitt@gmail.com>
2019-02-27 16:03:03 +0000

    Update Object

--- GEDCOMConverter.podspec
+++ GEDCOMConverter.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'GEDCOMConverter'
-  s.version          = '0.1.4'
+  s.version          = '0.1.6'
   s.summary          = 'With GEDCOMConverter, parsing a GEDCOM file to native Swift objects is too easy!'
 
 # This description is used to generate tags and improve search results.
--- GEDCOMConverter/Classes/Model/Individual/Object.swift
+++ GEDCOMConverter/Classes/Model/Individual/Object.swift
@@ -22,4 +22,21 @@ public class Object:Keyable,Codable, CustomStringConvertible {
   public var description: String {
     return ("Object: Title: \(title ?? q) file: \(file ?? q), Form: \(form ?? q)")
   }
+  /**Converts Ancestry URL to the URL of a JPG
+     //File comes from Ancestry like:
+     //http://trees.ancestry.com/rd?f=image&guid=1e1d842f-8619-4569-b320-f745ace26f19&tid=5973192&pid=1
+     //Need to convert to following format:
+     //https://mediasvc.ancestry.com/v2/image/namespaces/1093/media/1e1d842f-8619-4569-b320-f745ace26f19.jpg?client=Trees&imageQuality=hq
+     //See: https://support.ancestry.com/s/question/0D515000021Ob3rCAC/getting-images-via-ancestrycom-gedcom-files
+  **/
+  public var url:URL? {
+    guard let file = file,
+      let fileUrlComponents = URLComponents(string: file),
+      let fileQueryItems = fileUrlComponents.queryItems,
+      let guid = fileQueryItems.first(where: { $0.name == "guid" })?.value,
+      let form = form,
+      let newURL = URL(string: "https://mediasvc.ancestry.com/v2/image/namespaces/1093/media/\(guid).\(form)?client=Trees&imageQuality=hq")
+      else { return nil }
+    return newURL
+  }
 }
